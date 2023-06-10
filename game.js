02ffc2c7cc44aad147fe51c8e54142274e7d5b92
kyle7iao <101290819+kyle7iao@users.noreply.github.com>
2023-06-10 17:02:22 -0400

    Initialize index.html and game.js

---
+++ game.js
@@ -0,0 +1,43 @@
+// not sure if i should have global variables
+
+function getComputerChoice() {
+    let option;
+
+    // generate random choice
+    let randNum = Math.floor(Math.random() * 3)
+
+    switch (randNum) {
+        case 0:
+            option = 'rock';
+            break;
+        case 1:
+            option = 'paper';
+            break;
+        case 2:
+            option = 'scissors';
+            break;
+    }
+
+    console.log(option)
+    return option
+}
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper") 
+    ) {
+            return `You win! ${playerSelection} beats ${computerSelection}` 
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+    }
+}
+getComputerChoice();
